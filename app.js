2ce91e9c099db08e6472e1aa4c6891a720700e68
Lane Levesque <llevesque@i-opentech.com>
2022-08-19 12:00:43 -0700

    Second commit

---
+++ app.js
@@ -0,0 +1,12 @@
+define('Plugins/JQuery/JQuery.js', 'Plugins/Knockout/Knockout.js', ($, ko) => {
+    class Application {
+        constructor(_options) {
+            this.baseServiceUrl = 'https://pokeapi.co/api/v2/'
+        }
+
+        startApp() {
+
+        }
+        
+    }
+})

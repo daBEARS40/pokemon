12054d7db823967b2ed4cfa421ec3d6c352a1d3f
Lane Levesque <llevesque@i-opentech.com>
2022-11-03 21:43:58 -0700

    Added text plugin for require js

--- plugins/requireconfig.js
+++ plugins/requireconfig.js
@@ -1,8 +1,16 @@
 requirejs.config({
+  config: {
+    text: {
+      useXhr: function (url, protocol, hostname, port) {
+        return true;
+      },
+    },
+  },
   baseUrl: "http://127.0.0.1:5500/",
   paths: {
     jquery: "plugins/jquery",
     knockout: "plugins/knockout",
     komapping: "plugins/komapping",
+    text: "plugins/text",
   },
 });
